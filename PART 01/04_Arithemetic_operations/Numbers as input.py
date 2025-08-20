6e0bdfeb1f51e8658a62c572afb75ad3aa5da2cb
Enijeshiku <eenijeshiku@gmail.com>
2025-08-20 06:54:48 +0100

    Commit

---
+++ PART 01/04_Arithemetic_operations/Numbers as input.py
@@ -0,0 +1,27 @@
+# input() always gives you a string, even if the user types a number.
+
+# You can’t do math with strings directly.
+
+# To do calculations, you must cast (convert) the string into a number.
+
+
+input_str = input("Which year were you born? ")
+year = int(input_str)
+print(f"Your age at the end of the year 2021: {2025 - year}" )
+
+# Ask for year of birth
+input_str = input("What is your year of birth? ")
+
+# Convert the string into an integer
+year = int(input_str)
+
+# Do calculation (age in 2021)
+age = 2021 - year
+
+# Print result
+print("You were", age, "years old in 2021")
+
+
+# Reading the input with the input function and converting it with the int function can be achieved in one go:
+year = int(input("Which year were you born? "))
+print(f"Your age at the end of the year 2021: {2021 - year}" )
