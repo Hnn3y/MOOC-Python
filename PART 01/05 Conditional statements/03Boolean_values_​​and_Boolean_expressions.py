d10eab93b8d335c710e6553f55b2bbe20e71d5a1
Enijeshiku <eenijeshiku@gmail.com>
2025-08-27 10:03:09 +0100

    03

---
+++ PART 01/05 Conditional statements/03Boolean_values_​​and_Boolean_expressions.py
@@ -0,0 +1,6 @@
+# Any condition used in a Conditional statement will result in a truth value, that is, either true or false.
+
+# These types of values ​​are often called Boolean values
+
+# In Python they are handled by the booldata type. Variables of type boolcan only have two values: Trueor False.
+
