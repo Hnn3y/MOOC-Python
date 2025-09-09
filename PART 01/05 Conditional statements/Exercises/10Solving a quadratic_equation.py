15ffc298b160e813e973bb9495a9f0b2bab9b385
Enijeshiku <eenijeshiku@gmail.com>
2025-09-09 22:38:32 +0100

    Part 01_05 Exercise 10

---
+++ PART 01/05 Conditional statements/Exercises/10Solving a quadratic_equation.py
@@ -0,0 +1,21 @@
+# Write a program for Solving a quadratic equation of the form ax²+bx+c. 
+# The program asks for values ​​a, b and c. 
+# It should then use the quadratic formula to solve the equation. 
+# The quadratic formula expressed with the Python sqrtfunction is as follows:
+
+# x = (-b ± sqrt(b²-4ac))/(2a).
+
+# You can assume the equation will always have two real roots, so the above formula will always work.
+
+# An example of expected behavior:
+
+#Sample output
+#Value of a: 1 
+#Value of b: 2 
+#Value of c:-8
+#The roots are 2.0 and -4.0
+
+value_a = int(input("#Value of a: "))
+value_b = int(input("#Value of b: "))
+value_c = int(input("#Value of c: "))
+
