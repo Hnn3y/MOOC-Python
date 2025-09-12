e22deed65d777c20b0c6d4b0f91bcce9b3b14999
Enijeshiku <eenijeshiku@gmail.com>
2025-09-12 20:45:27 +0100

    Part 02

---
+++ PART 02/01_Programming terminology/01Statement.py
@@ -0,0 +1,13 @@
+# A statement is a part of the program which executes something. 
+# It often, but not always, refers to a single command.
+#For example, print("Hi!") is a statement which prints out a line of text. 
+# Likewise, number = 2 is a statement which assigns a value to a variable.
+#A statement can also be more complicated. 
+# It can, for instance, contain other statements. 
+# The following statement spans three lines:
+
+if name == "Anna":
+    print("Hi!")
+    number = 2
+    
+#In the above case there are two statements (a print statement and an assignment statement) within a conditional statement.
---
+++ PART 02/01_Programming terminology/02Block.py
@@ -0,0 +1,10 @@
+# A block is a group of consecutive statements that are at the same level in the structure of the program.
+
+if age > 17:
+    # beginning of the conditional block
+    print("You are of age!")
+    age = age + 1
+    print("You are now one year older...")
+    # end of the conditional block
+
+print("This here belongs to another block")
