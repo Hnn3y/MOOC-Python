77bdb1f2bfd1e43544461f5217fa2ed34f527b44
Enijeshiku <eenijeshiku@gmail.com>
2025-09-18 16:15:41 +0100

    docs(01Learning): add explanation for checking even

---
+++ PART 02/02_More conditionals/01Learning.py
@@ -0,0 +1,4 @@
+# The following example checks whether a number given by the user is even or not. 
+# Parity can be checked with the modulo operator %, which produces the remainder of an integer division operation. 
+# When divided by two, if the remainder is zero, the number is even. 
+# Otherwise the number is odd.
