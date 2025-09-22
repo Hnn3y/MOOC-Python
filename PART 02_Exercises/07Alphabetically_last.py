c66b815a1601f42424b17006ff98723c50e56512
Enijeshiku <eenijeshiku@gmail.com>
2025-09-22 16:32:13 +0100

    .

---
+++ PART 02_Exercises/07Alphabetically_last.py
@@ -0,0 +1,21 @@
+# write a program which asks the user for two words. 
+# The program should then print out whichever of the two comes alphabetically last.
+
+#You can assume all words will be typed in lowercase entirely.
+
+# Some examples of expected behaviour:
+
+1.
+#Please type in the 1st word: car
+#Please type in the 2nd word: scooter
+#scooter comes alphabetically last.
+
+2.
+#Please type in the 1st word: zorro
+#Please type in the 2nd word: batman
+#zorro comes alphabetically last.
+
+3.
+#Please type in the 1st word: python
+#Please type in the 2nd word: python
+#You gave the same word twice.
