19cfcc0e6d0d45e1aeb695e47aa5aa75707c147d
Enijeshiku <eenijeshiku@gmail.com>
2025-09-24 13:34:20 +0100

    feat(07Alphabetically_last): add program to compare two words and print the one that comes last alphabetically

--- PART 02_Exercises/07Alphabetically_last.py
+++ PART 02_Exercises/07Alphabetically_last.py
@@ -19,3 +19,13 @@
 #Please type in the 1st word: python
 #Please type in the 2nd word: python
 #You gave the same word twice.
+
+first_word = input("Please type in the 1st word: ")
+second_word = input("Please type in the 2nd word: ")
+
+if first_word == second_word:
+   print(f"You gave the same word twice.")
+elif first_word > second_word:
+   print(f"{first_word} comes alphabetically last.")
+else:
+   print(f"{second_word}comes alphabetically last.")
