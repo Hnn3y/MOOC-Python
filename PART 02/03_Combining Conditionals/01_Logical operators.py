9d687fe7b1bae10e7bcaae04b2978f50e4e0c779
Enijeshiku <eenijeshiku@gmail.com>
2025-09-24 14:39:30 +0100

    docs(logical operators): add explanation and truth table for and, or, not

---
+++ PART 02/03_Combining Conditionals/01_Logical operators.py
@@ -0,0 +1,14 @@
+#You can combine conditions with the logical operators 'and' and 'or'. 
+# The operator 'and' specifies that all the given conditions must be true at the same time. 
+# The operator 'or' specifies that at least one of the given conditions must be true.
+
+# The following truth table contains the behaviour of these operators in different situations:
+
+#a	       b	     a and b	a or b
+#False	False	  False	   False
+#True	   False	  False	   True
+#False	True	  False	   True
+#True	   True	  True	   True 
+
+# Sometimes it is necessary to know if something is not true. 
+# The operator 'not' negates a condition
