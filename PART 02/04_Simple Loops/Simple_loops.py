ed60a79e1c1d3b6f9c0e08614b9b29f9f2d7ea89
Enijeshiku <eenijeshiku@gmail.com>
2025-11-07 09:51:46 +0100

    .

--- PART 02/04_Simple Loops/Simple_loops.py
+++ PART 02/04_Simple Loops/Simple_loops.py
@@ -3,3 +3,6 @@
 # While conditional structures allow you to choose between sections of code, iteration structures allow you to repeat sections of code. 
 # They are often called loops because they allow the program to "loop back" to some line that was already executed before. 
 # The process of executing one repetition of a loop is also referred to as an iteration of the loop.
+
+
+# --> LOOPS AND HELPER VARIABLES
---
+++ PART 02_Exercises/018 Repeat Password.py
@@ -0,0 +1,14 @@
+password = input("Password: ")
+
+while True:
+   repeat = input("Repeat password: ")
+   if repeat != password:
+      print(f"They do not match!")
+   else:
+      print(f"User account created!")
+      break
+
+# write a program which asks the user for a password. 
+# The program should then ask the user to type in the password again. 
+# If the user types in something else than the first password, 
+# the program should keep on asking until the user types the first password again correctly.
