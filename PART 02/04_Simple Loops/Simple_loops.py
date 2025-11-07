09270e0efa606c8f67826aadffb5c3edba4e3bfd
Enijeshiku <eenijeshiku@gmail.com>
2025-11-07 11:09:04 +0100

    .

--- PART 02/04_Simple Loops/Simple_loops.py
+++ PART 02/04_Simple Loops/Simple_loops.py
@@ -6,3 +6,31 @@
 
 
 # --> LOOPS AND HELPER VARIABLES
+
+# The program uses two helper variables. The variable attempts keeps track of how many times the user has typed in a PIN. The variable success is set to either True or False based on whether the user is successful in signing in.
+
+attempts = 0
+
+while True:
+    code = input("Please type in your PIN: ")
+    attempts += 1
+
+    if code == "1234":
+        success = True
+        break
+
+    if attempts == 3:
+        success = False
+        break
+
+    # this is printed if the code was incorrect AND there have been less than three attempts
+    print("Incorrect...try again")
+
+if success:
+    print("Correct PIN entered!")
+else:
+    print("Too many attempts...")
+
+
+# --> Debugging print statements in loops
+
