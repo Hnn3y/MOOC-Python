9941bbf8b054870340a8bc7bb1f8310484dfa5cf
Enijeshiku <eenijeshiku@gmail.com>
2026-02-13 14:14:37 +0100

    Part 01

---
+++ 2026_PART 01_Exercises/02_SevenBrothers.py
@@ -0,0 +1,17 @@
+# Fix the code
+print("Simeoni")
+print("Juhani")
+print("Eero")
+print("Lauri")
+print("Aapo")
+print("Tuomas")
+print("Timo")
+
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
+
