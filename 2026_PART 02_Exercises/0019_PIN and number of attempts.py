a5793dcdfb3cef9f115834f1d67b874136d85c97
Henney <eenijeshiku@gmail.com>
2026-06-04 20:17:19 +0100

    Part 2 exercises

---
+++ 2026_PART 02_Exercises/0019_PIN and number of attempts.py
@@ -0,0 +1,3 @@
+# Please write a program which keeps asking the user for a PIN code until they type in the correct one, which is 4321 . 
+# The program should then print out the number of times the user tried different codes.
+
