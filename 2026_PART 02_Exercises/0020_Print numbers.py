4b91c1355dfcb1f7e95727a4d8ff9fc7e35dde7d
Henney <eenijeshiku@gmail.com>
2026-06-08 11:37:29 +0100

    Part 3 exercises

---
+++ 2026_PART 02_Exercises/0020_Print numbers.py
@@ -0,0 +1,8 @@
+# write a program which prints out all the even numbers between two and thirty, using a loop. 
+# Print each number on a separate line.
+
+number = 2
+
+while number <= 30:
+    print(number)
+    number += 2
